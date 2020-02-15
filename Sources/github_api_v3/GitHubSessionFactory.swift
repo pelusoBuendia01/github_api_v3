28c643153c9e18455ee7662e1c20a33227a698c6
pelusoBuendia01 <Francisco.romero.valle.01@gmail.com>
2020-02-15 01:33:49 -0600

    Update GitHubSessionFactory.swift

--- Sources/github_api_v3/GitHubSessionFactory.swift
+++ Sources/github_api_v3/GitHubSessionFactory.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class GitHubSessionFactory {
+public class GitHubSessionFactory {
     
     // MARK: TYPEALIAS
     //__________________________________________________________________________________________________________________
