cceebf68e6b9b21d12717b7e60bf0409d0031cea
pelusoBuendia01 <Francisco.romero.valle.01@gmail.com>
2020-02-16 14:37:12 -0600

    Development pull request (#14)
    * updationg pull request repositories README.md documentation
    
    * updationg pull request en entities README.md documentation
    
    * Updating team repositories base clases and protocols

--- Sources/github_api_v3/data/repositories/GHPullRequestRepo.swift
+++ Sources/github_api_v3/data/repositories/GHPullRequestRepo.swift
@@ -9,6 +9,108 @@ import Foundation
 
 internal class GHPullRequestRepo: PullRequestRepo {
     
+    
+    
+    // MARK: PRIVATE STATIC PROPERTIES
+    //__________________________________________________________________________________________________________________
+    
+        
+    // MARK: PUBLIC STATIC PROPERTIES
+    //__________________________________________________________________________________________________________________
+
+    
+    // MARK: PRIVATTE STATIC PROPERTIES
+    //__________________________________________________________________________________________________________________
+    
+    
+    // MARK: OVERRIDED PUBLIC PROPERTIES [UserRepository]
+    //__________________________________________________________________________________________________________________
+    
+    public var session  : GHSession
+    let reviews         : PullRequestReviewsRepo
+    let reviewComments  : PullRequestReviewCommentsRepo
+    let reviewRequest   : PullRequestReviewRequestRepo
+    
+    
+    // MARK: CONSTRUCTOR
+    //__________________________________________________________________________________________________________________
+    
+    init(session : GHSession) {
+        
+        /// initializa instance variables
+        self.session = session
+        
+        self.reviews        = GHPullRequestReviewsRepo          (session:self.session)
+        self.reviewComments = GHPullRequestReviewCommentsRepo   (session:self.session)
+        self.reviewRequest  = GHPullRequestReviewRequestRepo    (session:self.session)
+        
+    }
+    
+    
+    // MARK: OVERRIDE FUNCTIONS FROM [OrganizationRepository]
+    //__________________________________________________________________________________________________________________
+    
+    
+}
+
+internal class GHPullRequestReviewsRepo: PullRequestReviewsRepo {
+    
+    // MARK: PRIVATE STATIC PROPERTIES
+    //__________________________________________________________________________________________________________________
+    
+        
+    // MARK: PUBLIC STATIC PROPERTIES
+    //__________________________________________________________________________________________________________________
+    
+    // MARK: PRIVATTE STATIC PROPERTIES
+    //__________________________________________________________________________________________________________________
+    
+    // MARK: OVERRIDED PUBLIC PROPERTIES [UserRepository]
+    //__________________________________________________________________________________________________________________
+    public var session: GHSession
+    
+    // MARK: CONSTRUCTOR
+    //__________________________________________________________________________________________________________________
+    init(session : GHSession) {
+        self.session = session
+    }
+    
+    // MARK: OVERRIDE FUNCTIONS FROM [OrganizationRepository]
+    //__________________________________________________________________________________________________________________
+    
+}
+
+
+internal class GHPullRequestReviewCommentsRepo: PullRequestReviewCommentsRepo {
+    
+    // MARK: PRIVATE STATIC PROPERTIES
+    //__________________________________________________________________________________________________________________
+    
+        
+    // MARK: PUBLIC STATIC PROPERTIES
+    //__________________________________________________________________________________________________________________
+    
+    // MARK: PRIVATTE STATIC PROPERTIES
+    //__________________________________________________________________________________________________________________
+    
+    // MARK: OVERRIDED PUBLIC PROPERTIES [UserRepository]
+    //__________________________________________________________________________________________________________________
+    public var session: GHSession
+    
+    // MARK: CONSTRUCTOR
+    //__________________________________________________________________________________________________________________
+    init(session : GHSession) {
+        self.session = session
+    }
+    
+    // MARK: OVERRIDE FUNCTIONS FROM [OrganizationRepository]
+    //__________________________________________________________________________________________________________________
+    
+}
+
+
+internal class GHPullRequestReviewRequestRepo: PullRequestReviewRequestRepo {
+    
     // MARK: PRIVATE STATIC PROPERTIES
     //__________________________________________________________________________________________________________________
     
