1fc2293fee4324ff0c54eefd646e47880263f7e0
pelusoBuendia01 <Francisco.romero.valle.01@gmail.com>
2020-02-16 13:43:27 -0600

    Development organization (#10)
    Development organization

--- Sources/github_api_v3/data/entities/organizations/GHOrganizationAuthorizationEntity.swift
+++ Sources/github_api_v3/data/entities/organizations/GHOrganizationAuthorizationEntity.swift
@@ -2,21 +2,37 @@
 //  GHOrganizationAuthorizationEntity.swift
 //  githubAPIv3
 //
-//  Created by Francisco Romero on 03/02/20.
-//  Copyright © 2020 Francisco Romero. All rights reserved.
-//
+/// - copyright: <http://unlicense.org/>
+/// - author: francisco.romero.valle.01@gmail.com
+/// - seeAlso: https://developer.github.com/v3/orgs/
 
 import Foundation
 
 class GHOrganizationAuthorizationEntity: Codable {
-        
+
+    
     // MARK: ENUM
     //__________________________________________________________________________________________________________________
-    enum CodingKeys: CodingKey {
+    
+    enum CodingKeys: String, CodingKey {
+        case login                  = "login"
+        case credentialId           = "credential_id"
+        case credentialType         = "credential_type"
+        case tokenLastEight         = "token_last_eight"
+        case credentialAuthorizedAt = "credential_authorized_at"
+        case scopes                 = "scopes"
     }
-        
+    
+    
     // MARK: INSTANCE PUBLIC PROPERTIES
     //__________________________________________________________________________________________________________________
+    
+    let login                   : String
+    let credentialId            : String
+    let credentialType          : String
+    let tokenLastEight          : String
+    let credentialAuthorizedAt  : Date?
+    let scopes                  : [String]
         
         
     // MARK: OVERRIDE CONSTRUCTOR (Decodable)
@@ -25,9 +41,26 @@ class GHOrganizationAuthorizationEntity: Codable {
     required init(from decoder: Decoder) throws {
         
         /// Initialize local variables
-        //let container = try decoder.container(keyedBy: GHUserEntity.CodingKeys.self)
+        let container = try decoder.container(keyedBy: GHOrganizationAuthorizationEntity.CodingKeys.self)
         
         /// Initialize instance properties
+        do      { self.login = try container.decode(String.self, forKey: .login) }
+        catch   { self.login = "" }
+        
+        do      { self.credentialId = try container.decode(String.self, forKey: .credentialId)}
+        catch   { self.credentialId = "" }
+        
+        do      { self.credentialType = try container.decode(String.self, forKey: .credentialType)}
+        catch   { self.credentialType = "" }
+        
+        do      { self.tokenLastEight = try container.decode(String.self, forKey: .tokenLastEight)}
+        catch   { self.tokenLastEight = "" }
+        
+        do      { self.credentialAuthorizedAt = try container.decode(Date.self, forKey: .login)}
+        catch   { self.credentialAuthorizedAt = nil }
+        
+        do      { self.scopes = try container.decode([String].self, forKey: .login)}
+        catch   { self.scopes = [] }
         
     }
         
--- Sources/github_api_v3/data/repositories/README.md
+++ Sources/github_api_v3/data/repositories/README.md
@@ -4,17 +4,17 @@
 
 ### Protocols
 
-|Protocol name                                      | Description               |
-|:--------------------------------------------------|:--------------------------|
-|[GHAppRepos](./GHAppsRepo.swift)                   | TODO: 🏷 Add description  |
-|[GHIssuesRepo](./GHIssuesRepo.swift)               | TODO: 🏷 Add description  |
-|[GHMigrationRepo](./GHMigrationRepo.swift)         | TODO: 🏷 Add description  |
-|[GHMiscellaneousRepo](./GHMiscellaneousRepo.swift) | TODO: 🏷 Add description  |
-|[GHOrganizationRepo](./GHOrganizationRepository.swift)   | TODO: 🏷 Add description  |
-|[🌐GHProjectRepo](./GHProjectRepo.swift)             | Http access to project repository  |
-|[GHPullRequestRepo](./GHPullRequestRepo.swift)     | TODO: 🏷 Add description  |
-|[GHReactionsRepo](./GHReactionsRepo.swift)         | TODO: 🏷 Add description  |
-|[GHRepositoryRepo](./GHRepositoryRepository.swift) | TODO: 🏷 Add description  |
-|[GHSearchRepo](./GHSearchRepo.swift)               | TODO: 🏷 Add description  |
-|[GHTeamsRepo](./GHTeamsRepo.swift)                 | TODO: 🏷 Add description  |
-|[🌐GHUserRepo](./GHUserRepo.swift)             | Http access to project repository  |
+|Protocol name                                              | Description               |
+|:----------------------------------------------------------|:--------------------------|
+|[GHAppRepos](./GHAppsRepo.swift)                           | TODO: 🏷 Add description  |
+|[GHIssuesRepo](./GHIssuesRepo.swift)                       | TODO: 🏷 Add description  |
+|[GHMigrationRepo](./GHMigrationRepo.swift)                 | TODO: 🏷 Add description  |
+|[GHMiscellaneousRepo](./GHMiscellaneousRepo.swift)         | TODO: 🏷 Add description  |
+|[🌐 OrganizationRepo](./GHOrganizationRepository.swift)    | Http access to organization repository  |
+|[🌐 GHProjectRepo](./GHProjectRepo.swift)                  | Http access to project repository  |
+|[GHPullRequestRepo](./GHPullRequestRepo.swift)             | TODO: 🏷 Add description  |
+|[GHReactionsRepo](./GHReactionsRepo.swift)                 | TODO: 🏷 Add description  |
+|[GHRepositoryRepo](./GHRepositoryRepository.swift)         | TODO: 🏷 Add description  |
+|[GHSearchRepo](./GHSearchRepo.swift)                       | TODO: 🏷 Add description  |
+|[GHTeamsRepo](./GHTeamsRepo.swift)                         | TODO: 🏷 Add description  |
+|[🌐 GHUserRepo](./GHUserRepo.swift)                        | Http access to project repository  |
